55133078ff035707209d85e9a54200bc6521f8b2
akatuki1234 <79127845+akatuki1234@users.noreply.github.com>
2021-02-17 20:47:26 +0900

    Add files via upload
    雨が降っているHPです。

---
+++ 雨が降るHP/javascript.js
@@ -0,0 +1,248 @@
+// function rain(){
+//   const rain = document.getElementById('rain1');
+//    //乱数取得→Int型変換
+//   for(var i=1; i<=50; i++){
+//     let Rm =  parseInt((Math.random() * 300) + 1);
+//       console.log(Rm);
+//     if(Rm === 300){
+//       console.log("300ありました。")
+//     }
+//   }
+// }
+// rain();
+move1();
+move2();
+move3();
+move4();
+move6();
+move7();
+move8();
+move9();
+move10();
+move11();
+move12();
+move13();
+move14();
+move15();
+move16();
+move17();
+move18();
+move19();
+move20();
+
+function move1(){
+  let Rm =  parseInt((Math.random() * 4000) + 1); 
+  const rain = document.getElementById('rain1');
+    if(Rm >= 0 && Rm < 2000){
+    rain.style.left = Rm+"px";
+    }
+    else if(Rm >= 2000 && Rm < 4000){
+      rain.style.right = Rm+"px";
+    }
+}
+
+
+function move2(){
+  let Rm =  parseInt((Math.random() * 4000) + 1); 
+  const rain = document.getElementById('rain2');
+    if(Rm >= 0 && Rm < 2000){
+    rain.style.left = Rm+"px";
+    }
+    else if(Rm >= 2000 && Rm < 4000){
+      rain.style.right = Rm+"px";
+    }
+}
+
+function move3(){
+  let Rm =  parseInt((Math.random() * 4000) + 1); 
+  const rain = document.getElementById('rain3');
+    if(Rm >= 0 && Rm < 2000){
+    rain.style.left = Rm+"px";
+    }
+    else if(Rm >= 2000 && Rm < 4000){
+      rain.style.right = Rm+"px";
+    }
+}
+
+function move4(){
+  let Rm =  parseInt((Math.random() * 4000) + 1); 
+  const rain = document.getElementById('rain4');
+    if(Rm >= 0 && Rm < 2000){
+    rain.style.left = Rm+"px";
+    }
+    else if(Rm >= 2000 && Rm < 4000){
+      rain.style.right = Rm+"px";
+    }
+}
+
+function move5(){
+  let Rm =  parseInt((Math.random() * 4000) + 1); 
+  const rain = document.getElementById('rain5');
+    if(Rm >= 0 && Rm < 2000){
+    rain.style.left = Rm+"px";
+    }
+    else if(Rm >= 2000 && Rm < 4000){
+      rain.style.right = Rm+"px";
+    }
+}
+
+function move6(){
+  let Rm =  parseInt((Math.random() * 4000) + 1); 
+  const rain = document.getElementById('rain6');
+    if(Rm >= 0 && Rm < 2000){
+    rain.style.left = Rm+"px";
+    }
+    else if(Rm >= 2000 && Rm < 4000){
+      rain.style.right = Rm+"px";
+    }
+}
+
+function move7(){
+  let Rm =  parseInt((Math.random() * 4000) + 1); 
+  const rain = document.getElementById('rain7');
+    if(Rm >= 0 && Rm < 2000){
+    rain.style.left = Rm+"px";
+    }
+    else if(Rm >= 2000 && Rm < 4000){
+      rain.style.right = Rm+"px";
+    }
+}
+
+function move8(){
+  let Rm =  parseInt((Math.random() * 4000) + 1); 
+  const rain = document.getElementById('rain8');
+    if(Rm >= 0 && Rm < 2000){
+    rain.style.left = Rm+"px";
+    }
+    else if(Rm >= 2000 && Rm < 4000){
+      rain.style.right = Rm+"px";
+    }
+}
+
+function move9(){
+  let Rm =  parseInt((Math.random() * 4000) + 1); 
+  const rain = document.getElementById('rain9');
+    if(Rm >= 0 && Rm < 2000){
+    rain.style.left = Rm+"px";
+    }
+    else if(Rm >= 2000 && Rm < 4000){
+      rain.style.right = Rm+"px";
+    }
+}
+
+function move10(){
+  let Rm =  parseInt((Math.random() * 4000) + 1); 
+  const rain = document.getElementById('rain10');
+    if(Rm >= 0 && Rm < 2000){
+    rain.style.left = Rm+"px";
+    }
+    else if(Rm >= 2000 && Rm < 4000){
+      rain.style.right = Rm+"px";
+    }
+}
+
+function move11(){
+  let Rm =  parseInt((Math.random() * 4000) + 1); 
+  const rain = document.getElementById('rain11');
+    if(Rm >= 0 && Rm < 2000){
+    rain.style.left = Rm+"px";
+    }
+    else if(Rm >= 2000 && Rm < 4000){
+      rain.style.right = Rm+"px";
+    }
+}
+
+function move12(){
+  let Rm =  parseInt((Math.random() * 4000) + 1); 
+  const rain = document.getElementById('rain12');
+    if(Rm >= 0 && Rm < 2000){
+    rain.style.left = Rm+"px";
+    }
+    else if(Rm >= 2000 && Rm < 4000){
+      rain.style.right = Rm+"px";
+    }
+}
+
+function move13(){
+  let Rm =  parseInt((Math.random() * 4000) + 1); 
+  const rain = document.getElementById('rain13');
+    if(Rm >= 0 && Rm < 2000){
+    rain.style.left = Rm+"px";
+    }
+    else if(Rm >= 2000 && Rm < 4000){
+      rain.style.right = Rm+"px";
+    }
+}
+
+function move14(){
+  let Rm =  parseInt((Math.random() * 4000) + 1); 
+  const rain = document.getElementById('rain14');
+    if(Rm >= 0 && Rm < 2000){
+    rain.style.left = Rm+"px";
+    }
+    else if(Rm >= 2000 && Rm < 4000){
+      rain.style.right = Rm+"px";
+    }
+}
+
+function move15(){
+  let Rm =  parseInt((Math.random() * 4000) + 1); 
+  const rain = document.getElementById('rain15');
+    if(Rm >= 0 && Rm < 2000){
+    rain.style.left = Rm+"px";
+    }
+    else if(Rm >= 2000 && Rm < 4000){
+      rain.style.right = Rm+"px";
+    }
+}
+function move16(){
+  let Rm =  parseInt((Math.random() * 4000) + 1); 
+  const rain = document.getElementById('rain16');
+    if(Rm >= 0 && Rm < 2000){
+    rain.style.left = Rm+"px";
+    }
+    else if(Rm >= 2000 && Rm < 4000){
+      rain.style.right = Rm+"px";
+    }
+}
+
+function move17(){
+  let Rm =  parseInt((Math.random() * 4000) + 1); 
+  const rain = document.getElementById('rain17');
+    if(Rm >= 0 && Rm < 2000){
+    rain.style.left = Rm+"px";
+    }
+    else if(Rm >= 2000 && Rm < 4000){
+      rain.style.right = Rm+"px";
+    }
+}
+function move18(){
+  let Rm =  parseInt((Math.random() * 4000) + 1); 
+  const rain = document.getElementById('rain18');
+    if(Rm >= 0 && Rm < 2000){
+    rain.style.left = Rm+"px";
+    }
+    else if(Rm >= 2000 && Rm < 4000){
+      rain.style.right = Rm+"px";
+    }
+}
+function move19(){
+  let Rm =  parseInt((Math.random() * 4000) + 1); 
+  const rain = document.getElementById('rain19');
+    if(Rm >= 0 && Rm < 2000){
+    rain.style.left = Rm+"px";
+    }
+    else if(Rm >= 2000 && Rm < 4000){
+      rain.style.right = Rm+"px";
+    }
+}
+function move20(){
+  let Rm =  parseInt((Math.random() * 4000) + 1); 
+  const rain = document.getElementById('rain20');
+    if(Rm >= 0 && Rm < 2000){
+    rain.style.left = Rm+"px";
+    }
+    else if(Rm >= 2000 && Rm < 4000){
+      rain.style.right = Rm+"px";
+    }
+}
